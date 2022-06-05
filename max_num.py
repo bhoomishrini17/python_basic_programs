156447eea16cdaf4b25dd1ef799828b665a9bc83
bhoomishrini <106904822+bhoomishrini17@users.noreply.github.com>
2022-06-05 15:15:34 +0530

    Add files via upload

---
+++ max_num.py
@@ -0,0 +1,11 @@
+
+#maximum of two numbers 
+num1=float(input("enter two numbers\n"))
+num2=float(input())
+if(num1>num2):
+    print(str(num1)+" is bigger")
+else:
+    print(str(num2)+" is bigger")
+    
+#using max function
+print(str(max(num1,num2))+" is bigger")
