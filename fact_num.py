4ba8beef331c45411c1cc2d3f383303aa87a8357
bhoomishrini <106904822+bhoomishrini17@users.noreply.github.com>
2022-06-05 16:04:51 +0530

    Add files via upload

---
+++ fact_num.py
@@ -0,0 +1,23 @@
+#recursive solution
+num=int(input("enter the number to find its factorial\n"))
+def factorial(num):
+    if(num<0):
+        return 0
+    if(num==1 or num==0):
+        return 1
+    else:
+        return num*factorial(num-1)
+print("factorial of ",num, " is ",factorial(num))
+
+#iterative method
+i=num
+if(num<0):
+    print("factorial of a ",num, " is 0")
+if(num==1 or num==0):
+    print("factorial of a ",num, " is 1")
+else:
+    fact=1
+    while(i>1):
+        fact=fact*i
+        i=i-1
+print("factorial of ",num," is ",fact)
