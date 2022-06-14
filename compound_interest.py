34755eb684704bf862c2b26386d08627e52eba00
bhoomishrini <106904822+bhoomishrini17@users.noreply.github.com>
2022-06-14 23:58:23 +0530

    Create compound_interest.py

---
+++ compound_interest.py
@@ -0,0 +1,11 @@
+#using pow function
+
+def compound_interest(principle, rate, time):
+	Amount = principle * (pow((1 + rate / 100), time))
+	CI = Amount - principle
+	print("Compound interest is", CI)
+
+p=float(input("enter principal\n"))
+r=float(input("enter rate of interest\n"))
+t=float(input("enter time period\n"))
+compound_interest(p,r,t)
