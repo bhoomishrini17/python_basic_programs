dbbe1a351036e182e5cc32ebe02587bb75ff9433
bhoomishrini <106904822+bhoomishrini17@users.noreply.github.com>
2022-06-15 00:01:04 +0530

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -9,3 +9,15 @@ p=float(input("enter principal\n"))
 r=float(input("enter rate of interest\n"))
 t=float(input("enter time period\n"))
 compound_interest(p,r,t)
+
+
+#using **
+def compound_interest(principle, rate, time):
+	Amount = principle * (1+(rate/100))**time
+	CI = Amount - principle
+	print("Compound interest is", CI)
+
+p=float(input("enter principal\n"))
+r=float(input("enter rate of interest\n"))
+t=float(input("enter time period\n"))
+compound_interest(p,r,t)
